2ead96fe2fc27005b1f6c6ca06bc276ab8f1d9b4
phenixcxz <phenixcxz@163.com>
2020-03-31 17:37:07 +0800

    “调整电池显示“

--- Spoons/Battery.spoon/init.lua
+++ Spoons/Battery.spoon/init.lua
@@ -7,13 +7,6 @@
 local obj={}
 obj.__index = obj
 
--- Metadata
-obj.name = "SpeedMenu"
-obj.version = "1.0"
-obj.author = "ashfinal <ashfinal@gmail.com>"
-obj.homepage = "https://github.com/Hammerspoon/Spoons"
-obj.license = "MIT - https://opensource.org/licenses/MIT"
-
 function obj:init()
     self.menubar = hs.menubar.new()
     obj:rescan()
@@ -34,7 +27,11 @@ local function data_diff()
         end
     end
 
-    local disp_str = 'Tim:' .. obj.kbout  .. '\nCap:' .. obj.kbin
+ --   local powe = string.format("%2.2f",hs.battery.watts()) .. 'W'
+ --   local vol = string.format("%2.2f",hs.battery.voltage()/1000) .. 'V'
+
+   -- local disp_str = 'Cap:' .. obj.kbin .. '  Pow:' .. powe .. '\nTim:' .. obj.kbout .. '   Vol:' .. vol 
+    local disp_str = 'Cap:' .. obj.kbin  .. '\nTim:' .. obj.kbout  
     if obj.darkmode then
         obj.disp_str = hs.styledtext.new(disp_str, {font={size=9.0, color={hex="#FFFFFF"}}})
     else
@@ -49,41 +46,46 @@ function obj:rescan()
   --  obj.darkmode = hs.osascript.applescript('tell application "System Events"\nreturn dark mode of appearance preferences\nend tell')
     local menuitems_table = {}
     if obj.interface then
+        -- obj.nowcap = string.format("%3.0f", hs.battery.capacity()/hs.battery.maxCapacity()*100) .. ' %'
+
+        -- obj.health = string.format("%2.0f", hs.battery.maxCapacity()/hs.battery.designCapacity()*100) .. ' %'
+        -- table.insert(menuitems_table, {
+        --     title = "实际电量 " .. obj.nowcap
+
+        -- })
+        -- table.insert(menuitems_table, {
+        --     title = "当前功率 " .. hs.battery.watts() .. 'W'
+
+        -- })
+        -- table.insert(menuitems_table, {
+        --     title = "当前电流 " .. hs.battery.amperage() .. 'mA'
+
+        -- })
+        -- table.insert(menuitems_table, {
+        --     title = "当前电压" .. hs.battery.voltage() .. 'mV'
+        -- })        
+
+        -- table.insert(menuitems_table, {
+        --      title = "预设容量:" .. hs.battery.designCapacity() .. 'mAh'
+        --  })
+        -- table.insert(menuitems_table, {
+        --      title = "当前最大容量:" .. hs.battery.maxCapacity() .. 'mAh'
+        --  })
+        -- table.insert(menuitems_table, {
+        --      title = "电池健康:" .. obj.health
+        --  })
 
         if obj.timer then
             obj.timer:stop()
             obj.timer = nil
         end
-        obj.timer = hs.timer.doEvery(3, data_diff)
+        obj.timer = hs.timer.doEvery(1, data_diff)
+
     end
-    
-    obj.nowcap = string.format("%3.0f", hs.battery.capacity()/hs.battery.maxCapacity()*100) .. ' %'
-    table.insert(menuitems_table, {
-        title = "实际电量 " .. obj.nowcap
-
-    })   
-    table.insert(menuitems_table, {
-        title = "当前功率 " .. hs.battery.watts() .. 'W'
-
-    })
-    table.insert(menuitems_table, {
-        title = "当前电流 " .. hs.battery.amperage() .. 'mA'
-
-    })
-    table.insert(menuitems_table, {
-        title = "当前电压" .. hs.battery.voltage() .. 'mV'
-    })
-    obj.health = string.format("%2.0f", hs.battery.maxCapacity()/hs.battery.designCapacity()*100) .. ' %'
-    table.insert(menuitems_table, {
-         title = "预设容量:" .. hs.battery.designCapacity() .. 'mAh'
-     })
-    table.insert(menuitems_table, {
-         title = "当前最大容量:" .. hs.battery.maxCapacity() .. 'mAh'
-     })
-    table.insert(menuitems_table, {
-         title = "电池健康:" .. obj.health
-     })
-
+    -- table.insert(menuitems_table, {
+    --     title = "Rescan Interfaces",
+    --     fn = function() obj:rescan() end
+    -- })
     obj.menubar:setTitle("⚠︎")
     obj.menubar:setMenu(menuitems_table)
 end
--- Spoons/SpeedMenu.spoon/init.lua
+++ Spoons/SpeedMenu.spoon/init.lua
@@ -25,14 +25,14 @@ local function data_diff()
     local in_diff = in_seq - obj.inseq
     local out_diff = out_seq - obj.outseq
     if in_diff/1024 > 1024 then
-        obj.kbin = string.format("%6.2f", in_diff/1024/1024) .. ' mb/s'
+        obj.kbin = string.format("%4.2f", in_diff/1024/1024) .. ' mb/s'
     else
-        obj.kbin = string.format("%6.2f", in_diff/1024) .. ' kb/s'
+        obj.kbin = string.format("%4.2f", in_diff/1024) .. ' kb/s'
     end
     if out_diff/1024 > 1024 then
-        obj.kbout = string.format("%6.2f", out_diff/1024/1024) .. ' mb/s'
+        obj.kbout = string.format("%4.2f", out_diff/1024/1024) .. ' mb/s'
     else
-        obj.kbout = string.format("%6.2f", out_diff/1024) .. ' kb/s'
+        obj.kbout = string.format("%4.2f", out_diff/1024) .. ' kb/s'
     end
     local disp_str = '⥄ ' .. obj.kbout .. '\n⥂ ' .. obj.kbin
     if obj.darkmode then
