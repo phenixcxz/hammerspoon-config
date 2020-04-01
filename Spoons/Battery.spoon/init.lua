aa23bdca7cc90afcb5a18f6fe1ba7adea9fa639b
phenixcxz <phenixcxz@163.com>
2020-04-01 09:59:05 +0800

    增加充电指示

--- Spoons/Battery.spoon/init.lua
+++ Spoons/Battery.spoon/init.lua
@@ -1,9 +1,3 @@
---- === SpeedMenu ===
----
---- Menubar netspeed meter
----
---- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpeedMenu.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpeedMenu.spoon.zip)
-
 local obj={}
 obj.__index = obj
 
@@ -18,9 +12,16 @@ local function data_diff()
     --剩余时间计算
 
     if (hs.battery.isCharging()) then 
-        obj.kbout = string.format("%2.0f:%02d",hs.battery.timeToFullCharge()/60-0.5,hs.battery.timeToFullCharge()%60) 
+        obj.kbin = obj.kbin .. '充'
+        if(hs.battery.timeToFullCharge() >0) then
+            obj.kbout = string.format("%2.0f:%02d",hs.battery.timeToFullCharge()/60-0.5,hs.battery.timeToFullCharge()%60) .. '电'
+        elseif(hs.battery.timeToFullCharge() == 0) then
+            obj.kbout = string.format("%2.0f:%02d",hs.battery.timeToFullCharge()/60-0.5,hs.battery.timeToFullCharge()%60) .. '满'
+        else
+            obj.kbout= ' ---- 电'  
+        end
     elseif (hs.battery.timeRemaining() < 0) then
-        obj.kbout = '计算中'
+        obj.kbout = ' ---- '
     elseif(hs.battery.powerSource() == "Battery Power" ) then 
         obj.kbout = string.format("%2.0f:%02d",hs.battery.timeRemaining()/60-0.5,hs.battery.timeRemaining()%60)
     end
@@ -41,35 +42,38 @@ end
 
 
 populateMenu = function(key)
-    setTitle() -- Update the counter every time the menu is refreshed
+    --setTitle() -- Update the counter every time the menu is refreshed
     nowcap = string.format("%3.0f", hs.battery.capacity()/hs.battery.maxCapacity()*100) .. ' %'
 
     health = string.format("%2.0f", hs.battery.maxCapacity()/hs.battery.designCapacity()*100) .. ' %'
     menuData = {}
     table.insert(menuData, {
-        title = "实际电量 " .. nowcap
+        title = "实际电量：" .. nowcap
 
     })
     table.insert(menuData, {
-        title = "当前功率 " .. hs.battery.watts() .. 'W'
+        title = "当前功率：" .. hs.battery.watts() .. 'W'
 
     })
     table.insert(menuData, {
-        title = "当前电流 " .. hs.battery.amperage() .. 'mA'
+        title = "当前电流：" .. hs.battery.amperage() .. 'mA'
 
     })
     table.insert(menuData, {
-        title = "当前电压" .. hs.battery.voltage() .. 'mV'
+        title = "当前电压：" .. hs.battery.voltage() .. 'mV'
     })        
 
     table.insert(menuData, {
-         title = "预设容量:" .. hs.battery.designCapacity() .. 'mAh'
+         title = "预设容量：" .. hs.battery.designCapacity() .. 'mAh'
      })
     table.insert(menuData, {
-         title = "当前最大容量:" .. hs.battery.maxCapacity() .. 'mAh'
+         title = "最大容量：" .. hs.battery.maxCapacity() .. 'mAh'
      })
     table.insert(menuData, {
-         title = "电池健康:" .. health
+         title = "当前电量：" .. hs.battery.capacity() .. 'mAh'
+     })
+    table.insert(menuData, {
+         title = "电池健康：" .. health
      })
     return menuData
 end
@@ -77,7 +81,8 @@ end
 function obj:rescan()
     obj.menubar:setMenu(populateMenu)
     data_diff()
-    hs.timer.doEvery(30, data_diff)
+    timer = hs.timer.new(5, data_diff)
+    timer:start()
 end
 
 return obj
