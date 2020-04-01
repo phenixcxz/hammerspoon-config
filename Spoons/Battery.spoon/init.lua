1e649b97a706b2f3350594c5a3ed7db175841278
phenixcxz <phenixcxz@163.com>
2020-04-01 10:22:15 +0800

    修复bug

--- Spoons/Battery.spoon/init.lua
+++ Spoons/Battery.spoon/init.lua
@@ -6,30 +6,34 @@ function obj:init()
     obj:rescan()
 end
 
+
 local function data_diff()
     --电量百分比计算
     obj.kbin = string.format("%3.0f",hs.battery.percentage()) .. '%'
     --剩余时间计算
 
-    if (hs.battery.isCharging()) then 
+    if (hs.battery.isCharging()) then   --充电中
         obj.kbin = obj.kbin .. '充'
-        if(hs.battery.timeToFullCharge() >0) then
-            obj.kbout = string.format("%2.0f:%02d",hs.battery.timeToFullCharge()/60-0.5,hs.battery.timeToFullCharge()%60) .. '电'
-        elseif(hs.battery.timeToFullCharge() == 0) then
-            obj.kbout = string.format("%2.0f:%02d",hs.battery.timeToFullCharge()/60-0.5,hs.battery.timeToFullCharge()%60) .. '满'
+        local timeToFull = hs.battery.timeToFullCharge()
+        local timeM = timeToFull%60
+        local timeH = (timeToFull-timeM)/60
+
+        if(timeToFull >0) then
+            obj.kbout = string.format("%2.0f:%02d",timeH,timeM) .. '电'      --未充满
+        elseif(hs.battery.timeToFullCharge() == 0) then         --充满
+            obj.kbout = string.format("%2.0f:%02d",timeH,timeM) .. '满'
         else
-            obj.kbout= ' ---- 电'  
+            obj.kbout= ' ---- 电'  --计算中
         end
-    elseif (hs.battery.timeRemaining() < 0) then
+    elseif (hs.battery.timeRemaining() < 0) then    --没有充电，计算中
         obj.kbout = ' ---- '
-    elseif(hs.battery.powerSource() == "Battery Power" ) then 
-        obj.kbout = string.format("%2.0f:%02d",hs.battery.timeRemaining()/60-0.5,hs.battery.timeRemaining()%60)
+    elseif(hs.battery.powerSource() == "Battery Power" ) then   --剩余电量
+        local timeReamin = hs.battery.timeRemaining()
+        local timeRm = timeReamin%60
+        local timeRh = (timeReamin- timeRm)/60
+        obj.kbout = string.format("%2.0f:%02d",timeRh,timeRh)
     end
 
- --   local powe = string.format("%2.2f",hs.battery.watts()) .. 'W'
- --   local vol = string.format("%2.2f",hs.battery.voltage()/1000) .. 'V'
-
-   -- local disp_str = 'Cap:' .. obj.kbin .. '  Pow:' .. powe .. '\nTim:' .. obj.kbout .. '   Vol:' .. vol 
     local disp_str = 'Cap:' .. obj.kbin  .. '\nTim:' .. obj.kbout  
     if obj.darkmode then
         obj.disp_str = hs.styledtext.new(disp_str, {font={size=9.0, color={hex="#FFFFFF"}}})
