2aec96c1dfe83ddbb8c3e5e0685ac86888abe5fc
phenixcxz <phenixcxz@163.com>
2020-04-12 15:19:09 +0800

    增加系统启动时间检测

--- Spoons/Battery.spoon/init.lua
+++ Spoons/Battery.spoon/init.lua
@@ -3,7 +3,7 @@ obj.__index = obj
 
 -- #define lua_number2int(i,d) \
 --   { volatile union luai_Cast u; u.l_d = (d) + 6755399441055744.0; (i) = u.l_l; }
-
+local battery
 local h1 = hs.timer.hours(1)
 local d1 = hs.timer.days(1)
 local m1 = hs.timer.minutes(1)
@@ -12,26 +12,30 @@ local batTime1 = hs.timer.secondsSinceEpoch()
 local batTime2 = batTime1
 local batTime = 0
 
-
+--image1 = "......111.......\n......111.......\n1..............1\n1..............1\n"
 function obj:init()
     self.menubar = hs.menubar.new()
-    hs.battery.getAll()
+ --   hs.battery.getAll()
     obj:rescan()
+
 end
 
 
 local function data_diff()
-    --电量百分比计算
-    obj.kbin = string.format("%3.0f",hs.battery.percentage()) .. '%'
+
+    battery = hs.battery.getAll()
+
+    --电量百分比计算   
+    obj.kbin = string.format("%3.0f",battery.percentage) .. '%'
     --剩余时间计算
 
-    if (hs.battery.amperage()>0) then   --充电中
+    if (battery.amperage>0) then   --充电中
         batTime1 = 0;       --使用电池时间归零
-        local timeToFull = hs.battery.timeToFullCharge()
+        local timeToFull = battery.timeToFullCharge
         local timeM = timeToFull%60
         local timeH = (timeToFull-timeM)/60
         obj.kbout = string.format("%02d:%02d",timeH,timeM)
-    elseif (hs.battery.timeRemaining() < 0) then    --没有充电，计算中 
+    elseif (battery.timeRemaining < 0) then    --没有充电，计算中 
 
         if(batTime1 == 0) then 
             batTime1 = hs.timer.secondsSinceEpoch()     --记录开始使用电池时间
@@ -39,11 +43,11 @@ local function data_diff()
 
         obj.kbout = ' ----- '
 
-    elseif(hs.battery.powerSource() == "Battery Power" ) then   --剩余电量
+    elseif(battery.powerSource == "Battery Power" ) then   --剩余电量
         -- if(batTime1 == 0) then 
         --     batTime1 = hs.timer.secondsSinceEpoch()
         -- end
-        local timeReamin = hs.battery.timeRemaining()
+        local timeReamin = battery.timeRemaining
         local timeRm = timeReamin%60
         local timeRh = (timeReamin- timeRm)/60
         obj.kbout = string.format("%2d:%02d",timeRh,timeRm)
@@ -51,8 +55,8 @@ local function data_diff()
 
     --充电状态指示
     local disp_str
-    if (hs.battery.amperage()>0) then
-        if(hs.battery.timeToFullCharge() > 0) then 
+    if (battery.amperage>0) then
+        if(battery.timeToFullCharge > 0) then 
             disp_str = '充Cap:' .. obj.kbin  .. '\n电Tim:' .. obj.kbout  
         else
             disp_str = '充Cap:' .. obj.kbin  .. '\n满Tim:' .. obj.kbout   
@@ -66,10 +70,18 @@ local function data_diff()
     else
         obj.disp_str = hs.styledtext.new(disp_str, {font={size=9.0, color={hex="#000000"}}})
     end
+
+  --  image = hs.image.imageFromASCII(image1,table1)
+    obj.menubar:setIcon(image)
+
     obj.menubar:setTitle(obj.disp_str)
 end
+table1 = {
+    antialias=0,
+    lineWidth = 20
 
-hs.timer.absoluteTime()
+}
+-- hs.image.imageFromASCII(ascii[, context])
 
 
 
@@ -114,25 +126,25 @@ populateMenu = function(key)
 
     })
     table.insert(menuData, {
-        title = "当前功率：" .. hs.battery.watts() .. 'W'
+        title = "当前功率：" .. battery.watts .. 'W'
 
     })
     table.insert(menuData, {
-        title = "当前电流：" .. hs.battery.amperage() .. 'mA'
+        title = "当前电流：" .. battery.amperage .. 'mA'
 
     })
     table.insert(menuData, {
-        title = "当前电压：" .. hs.battery.voltage() .. 'mV'
+        title = "当前电压：" .. battery.voltage .. 'mV'
     })        
 
     table.insert(menuData, {
-         title = "预设容量：" .. hs.battery.designCapacity() .. 'mAh'
+         title = "预设容量：" .. battery.designCapacity .. 'mAh'
      })
     table.insert(menuData, {
-         title = "最大容量：" .. hs.battery.maxCapacity() .. 'mAh'
+         title = "最大容量：" .. battery.maxCapacity .. 'mAh'
      })
     table.insert(menuData, {
-         title = "当前电量：" .. hs.battery.capacity() .. 'mAh'
+         title = "当前电量：" .. battery.capacity .. 'mAh'
      })
     table.insert(menuData, {
          title = "电池健康：" .. health
--- init.lua
+++ init.lua
@@ -11,13 +11,14 @@ require "sleep.sleep"		--睡眠wifi蓝牙管理
 --require "translate.translate"
 --require "statuslets.statuslets"
 --require "volume.volume"
---require "battery.battery"
+--AClock.spoonrequire "battery.battery"
 --require "speaker.speaker"
 --require "audio.audio"						  	
 --hs.loadSpoon('Calendar')
 --hs.loadSpoon('DeepLTranslate')
 hs.loadSpoon('SpeedMenu')	--网速显示
 hs.loadSpoon('Battery')		--电池信息显示
+print(hs.battery.getAll())
 --hs.loadSpoon('HSearch')
 
 
