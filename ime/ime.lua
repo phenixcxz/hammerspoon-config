8ee9acfa25ca9a4a2d1e216f1cbf4fbdd4e0d033
cxz <vcx@cxzs-MacBook-Air.local>
2020-09-30 15:12:54 +0800

    调整输入法

--- ime/ime.lua
+++ ime/ime.lua
@@ -1,11 +1,22 @@
+
+methodName = "搜狗拼音"
+
 local function Chinese()
-    hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+    currentMethod =hs.keycodes.currentMethod()
+    if  currentMethod ~= methodName then
+        hs.keycodes.setMethod(methodName)
+        -- updateFocusAppInputMethod()
+        -- hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+    --     -- print("设置输入法")
+    --     break
+    end
+    -- hs.keycodes.setMethod("com.sogou.inputmethod.sogou.pinyin")
 end
 
 --Chinese()
--- --local function Chinese()
--- --    hs.keycodes.currentSourceID("com.baidu.inputmethod.BaiduIM.pinyin")
--- --end
+-- local function Chinese()
+--    hs.keycodes.currentSourceID("com.baidu.inputmethod.BaiduIM.pinyin")
+-- end
 
 -- local function English()
 --     hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
@@ -39,43 +50,44 @@ local app2Ime = {
     {'Applications/Notion.app','Chinese'}
 }
 
-function updateFocusAppInputMethod()
-    -- local ime = 'English'
-    -- local focusAppPath = hs.window.frontmostWindow():application():path()
-    -- for index, app in pairs(app2Ime) do
-    --     local appPath = app[1]
-    --     local expectedIme = app[2]
+-- function updateFocusAppInputMethod()
+--     -- local ime = 'English'
+--     -- local focusAppPath = hs.window.frontmostWindow():application():path()
+--     -- for index, app in pairs(app2Ime) do
+--     --     local appPath = app[1]
+--     --     local expectedIme = app[2]
 
-    --     if focusAppPath == appPath then
-    --         ime = expectedIme
-    --         break
-    --     end
-    -- end
+--     --     if focusAppPath == appPath then
+--     --         ime = expectedIme
+--     --         break
+--     --     end
+--     -- end
 
-    -- if ime == 'English' then
-    --    English()
-    -- else
-        Chinese()
-    -- end
-end
+--     -- if ime == 'English' then
+--     --    English()
+--     -- else
+--         Chinese()
+--     -- end
+-- end
 
 -- helper hotkey to figure out the app path and name of current focused window
-hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
-    hs.alert.show("App path:        "
-    ..hs.window.focusedWindow():application():path()
-    .."\n"
-    .."App name:      "
-    ..hs.window.focusedWindow():application():name()
-    .."\n"
-    .."IM source id:  "
-    ..hs.keycodes.currentSourceID())
-end)
+-- hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
+--     hs.alert.show("App path:        "
+--     ..hs.window.focusedWindow():application():path()
+--     .."\n"
+--     .."App name:      "
+--     ..hs.window.focusedWindow():application():name()
+--     .."\n"
+--     .."IM source id:  "
+--     ..hs.keycodes.currentSourceID())
+-- end)
 
 -- Handle cursor focus and application's screen manage.
 function applicationWatcher(appName, eventType, appObject)
-    if (eventType == hs.application.watcher.activated) then
-        updateFocusAppInputMethod()
-    end
+    -- if (eventType == hs.application.watcher.activated) then
+    --     updateFocusAppInputMethod()
+    -- end
+    Chinese()
 end
 
 appWatcher = hs.application.watcher.new(applicationWatcher)
--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@
 --require "headphone.headphone"
 --require "hotkey.hotkey"		--应用启动快捷键设置
 require "ime.ime"			--自动切换输入法
---require "reload.reload"
+require "reload.reload"
 --require "usb.usb"
 require "window.window"		--快捷键调节窗口大小
 --require "wifi.wifi"
-require "clipboard.clipboard"	--剪切板记录
+--require "clipboard.clipboard"	--剪切板记录
 --require "sleep.sleep"		--睡眠wifi蓝牙管理
 --require "translate.translate"
 --require "statuslets.statuslets"
